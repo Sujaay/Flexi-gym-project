efae3a17427801432eb6f2d2e2d4fb5654181668
Sujaay <sujay.kumar47@gmail.com>
2023-10-05 13:03:19 +0530

    component about comments and content

--- src/components/about/First.jsx
+++ src/components/about/First.jsx
@@ -1,17 +1,25 @@
+// Importing styles from the about.module.css file
 import styles from "./about.module.css";
 
+// Defining a functional component named First
 const First = () => {
+  // Returning a JSX element
   return (
     <div
+      // Adding classes using template literals and styles imported from CSS module
       className={`${styles.first} margin-sections main-background`}
-      data-aos="zoom-out-down">
+      // Adding data-aos attribute for animation
+      data-aos="zoom-out-down"
+    >
+      {/* Rendering a heading */}
       <h1 className="main-heading">
         Start with us the body and
         <br />
-        mind clensing
+        mind cleansing
       </h1>
     </div>
   );
 };
 
+// Exporting the First component as the default export
 export default First;
--- src/components/about/Four.jsx
+++ src/components/about/Four.jsx
@@ -2,12 +2,16 @@ import { useNavigate } from "react-router";
 import styles from "./about.module.css";
 
 const Four = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Initializing the useNavigate hook
 
   return (
     <div className={styles.four}>
+      {/* Section with background image */}
       <div className={styles.four_image}></div>
+
+      {/* Values Section */}
       <div className={`${styles.four_content} sections-padding`}>
+        {/* Heading and Book a Class Button */}
         <div className={`${styles.values} container`}>
           <div>
             <p className="paragraph">Values</p>
@@ -15,7 +19,10 @@ const Four = () => {
           </div>
           <button onClick={() => navigate("/contact")}>Book a Class</button>
         </div>
+
+        {/* Info Boxes */}
         <div className={`${styles.four_boxes} container`}>
+          {/* Certified Trainer */}
           <div data-aos="fade-right">
             <i className="fa-regular fa-newspaper"></i>
             <h3>Certified trainer</h3>
@@ -24,6 +31,8 @@ const Four = () => {
               domination.
             </p>
           </div>
+
+          {/* Nutrition & Diet */}
           <div data-aos="fade-right">
             <i className="fa-solid fa-apple-whole"></i>
             <h3>Nutrition & diet</h3>
@@ -32,6 +41,8 @@ const Four = () => {
               domination.
             </p>
           </div>
+
+          {/* Years of Experience */}
           <div data-aos="fade-right">
             <i className="fa-solid fa-person"></i>
             <h3>Years of experience</h3>
@@ -42,19 +53,28 @@ const Four = () => {
           </div>
         </div>
       </div>
+
+      {/* Numbers Section */}
       <div className={`${styles.numbers} container`}>
+        {/* Experience */}
         <div>
           <h3>10+</h3>
           <p>Year of Experience</p>
         </div>
+
+        {/* Happy Clients */}
         <div>
           <h3>500+</h3>
           <p>Happy Clients</p>
         </div>
+
+        {/* Expert Trainers */}
         <div>
           <h3>50+</h3>
           <p>Expert Trainers</p>
         </div>
+
+        {/* Instagram Followers */}
         <div>
           <h3>15k</h3>
           <p>Instagram followers</p>
