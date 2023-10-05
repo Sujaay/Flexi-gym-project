7e523e2e763e23c932736a3d949ceca3f15810ca
Vedanshi Sahu <92024144+11roxanne@users.noreply.github.com>
2023-10-05 12:25:16 +0530

    Update Second.jsx

--- src/components/classes/Second.jsx
+++ src/components/classes/Second.jsx
@@ -1,5 +1,4 @@
 import styles from "./classes.module.css";
-
 import photo1 from "../../assets/home4.png";
 import photo2 from "../../assets/home2.png";
 import photo3 from "../../assets/home3.png";
@@ -7,49 +6,50 @@ import photo4 from "../../assets/classes2.png";
 import photo5 from "../../assets/classes3.png";
 import photo6 from "../../assets/classes4.png";
 import { Link } from "react-router-dom";
+// In this file we have created cards inside which there id detail information about the classes .
 
 const Second = () => {
   return (
-    <div className={`${styles.second} container sections-padding`}>
+    <div className={`${styles.second} container sections-padding`}>  //styles.second add applies css to the element
       <div className={styles.second_text}>
         <p className="paragraph">Our Trainers</p>
         <h2>We Trained You to Gain</h2>
       </div>
-      <div className={styles.classes}>
-        <div data-aos="fade-right">
+      <div className={styles.classes}>  
+        <div data-aos="fade-right">   //data-aos add the annimation the element
           <img src={photo1} alt="" />
           <h3>Pilates Training</h3>
-          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <p>Improve flexibility and core strength with our Pilates Training classes.</p>
           <Link to={"/yogaClass"}>Read More</Link>
         </div>
         <div data-aos="fade-right">
           <img src={photo2} alt="" />
           <h3>CrossFit</h3>
-          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <p>Experience high-intensity workouts and CrossFit training for fitness enthusiasts.</p>
           <Link to={"/singleClass"}>Read More</Link>
         </div>
         <div data-aos="fade-right">
           <img src={photo3} alt="" />
           <h3>Aerobic</h3>
-          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <p>Join our Aerobic classes to boost cardiovascular endurance and stamina.</p>
           <a href="#">Read More</a>
         </div>
         <div data-aos="fade-right">
           <img src={photo4} alt="" />
           <h3>Yoga As Therapy</h3>
-          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <p>Discover the therapeutic benefits of yoga with our Yoga As Therapy program.</p>
           <a href="#">Read More</a>
         </div>
         <div data-aos="fade-right">
           <img src={photo5} alt="" />
           <h3>Boxing</h3>
-          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <p>Get in shape and learn boxing techniques with our Boxing training sessions.</p>
           <a href="#">Read More</a>
         </div>
         <div data-aos="fade-right">
           <img src={photo6} alt="" />
           <h3>Energy Dance</h3>
-          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <p>Experience the energy of dance and fitness in our Energy Dance classes.</p>
           <Link to={"/yogaClass"}>Read More</Link>
         </div>
       </div>
@@ -58,3 +58,4 @@ const Second = () => {
 };
 
 export default Second;
+
